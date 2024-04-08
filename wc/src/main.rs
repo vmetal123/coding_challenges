d0c5527ffb7960df8bc85e2cfdc9eb2736870680
vmetal123 <bladi22@yahoo.com>
2024-04-08 16:51:56 -0600

    feat: implemented step five

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -7,50 +7,77 @@ use std::{
 fn main() {
     let args: Vec<_> = env::args().collect();
 
+    let file_name: &str;
+
     if args.len() < 4 {
-        println!("Error getting args");
+        file_name = &args[2];
+
+        let size = get_file_total_bytes(&file_name.to_string());
+        let lines_count = get_file_lines_count(&file_name.to_string());
+        let words_count = get_file_words_count(&file_name.to_string());
+
+        println!("{} {} {} {}", size, lines_count, words_count, file_name);
+
         return ();
     }
 
+    file_name = &args[3];
     let command = &args[2];
-    let file_name = &args[3];
 
     if command == "-c" {
-        let size = fs::metadata(file_name).unwrap().len();
+        let size = get_file_total_bytes(&file_name.to_string());
 
         println!("{} {}", size, file_name);
     }
 
     if command == "-l" {
-        let file = File::open(file_name).unwrap();
-        let buffered = BufReader::new(file);
+        let lines_count = get_file_lines_count(&file_name.to_string());
 
-        println!("{:?} {}", buffered.lines().count(), file_name);
+        println!("{:?} {}", lines_count, file_name);
     }
 
     if command == "-w" {
-        let file = File::open(file_name).unwrap();
-        let buffered = BufReader::new(file);
+        let words_count: usize = get_file_words_count(&file_name.to_string());
 
-        let words_count: usize = buffered
-            .lines()
-            .map(|line| line.unwrap().split_whitespace().count())
-            .sum();
-
-        println!("{}", words_count);
+        println!("{} {}", words_count, file_name);
     }
 
     if command == "-m" {
-        let file = File::open(file_name).unwrap();
-        let buffered = BufReader::new(file);
+        let chars_count = get_file_chars_count(&file_name.to_string());
 
-        let chars_count: usize = buffered
-            .lines()
-            .map(|line| line.unwrap().chars().count())
-            .sum();
-
-        println!("{}", chars_count);
+        println!("{} {}", chars_count, file_name);
     }
 
     ()
+}
+
+fn get_file_total_bytes(file_name: &String) -> u64 {
+    fs::metadata(&file_name).unwrap().len()
+}
+
+fn get_file_words_count(file_name: &String) -> usize {
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line| line.unwrap().split_whitespace().count())
+        .sum()
+}
+
+fn get_file_lines_count(file_name: &String) -> usize {
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    reader.lines().count()
+}
+
+fn get_file_chars_count(file_name: &String) -> usize {
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line| line.unwrap().chars().count())
+        .sum()
 }
